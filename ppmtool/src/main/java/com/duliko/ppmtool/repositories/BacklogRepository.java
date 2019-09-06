622beedf7adad64b37814300a9ecec543a979dde
Michele Dexter <micheleannedexter@gmail.com>
2019-09-06 12:56:03 -0400

    backlog repository

---
+++ ppmtool/src/main/java/com/duliko/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package com.duliko.ppmtool.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.duliko.ppmtool.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends JpaRepository<Backlog, Long> {
+
+}
