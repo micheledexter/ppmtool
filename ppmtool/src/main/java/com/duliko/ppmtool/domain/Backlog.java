5b2864c3ef13a68161e6c698597210938b8a77eb
Michele Dexter <micheleannedexter@gmail.com>
2019-09-06 12:55:32 -0400

    backlog domain object model

---
+++ ppmtool/src/main/java/com/duliko/ppmtool/domain/Backlog.java
@@ -0,0 +1,122 @@
+package com.duliko.ppmtool.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Backlog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer PTSequence = 0;
+	private String projectIdentifier;
+	
+	// OneToOne with project
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "project_id", nullable = false)
+	private Project project;
+	
+	// OneToMany with projectTasks
+	
+	// ECLIPSE CORSH
+
+	public Backlog() {
+		super();
+	}
+
+	public Backlog(Long id, Integer pTSequence, String projectIdentifier, Project project) {
+		super();
+		this.id = id;
+		PTSequence = pTSequence;
+		this.projectIdentifier = projectIdentifier;
+		this.project = project;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getPTSequence() {
+		return PTSequence;
+	}
+
+	public void setPTSequence(Integer pTSequence) {
+		PTSequence = pTSequence;
+	}
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	@Override
+	public String toString() {
+		return "Backlog [id=" + id + ", PTSequence=" + PTSequence + ", projectIdentifier=" + projectIdentifier
+				+ ", project=" + project + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((PTSequence == null) ? 0 : PTSequence.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((project == null) ? 0 : project.hashCode());
+		result = prime * result + ((projectIdentifier == null) ? 0 : projectIdentifier.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Backlog other = (Backlog) obj;
+		if (PTSequence == null) {
+			if (other.PTSequence != null)
+				return false;
+		} else if (!PTSequence.equals(other.PTSequence))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (project == null) {
+			if (other.project != null)
+				return false;
+		} else if (!project.equals(other.project))
+			return false;
+		if (projectIdentifier == null) {
+			if (other.projectIdentifier != null)
+				return false;
+		} else if (!projectIdentifier.equals(other.projectIdentifier))
+			return false;
+		return true;
+	}
+}
